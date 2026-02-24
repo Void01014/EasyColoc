a7d6990ae9b67eae9fb4340cae9fcd014bc67d05
Omar EL FILALI <omarelfiie2007@gmail.com>
2026-02-24 12:10:11 +0000

    I installed breeze

---
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Livewire\Forms\LoginForm;
+use Illuminate\Support\Facades\Session;
+
+use function Livewire\Volt\form;
+use function Livewire\Volt\layout;
+
+layout('layouts.guest');
+
+form(LoginForm::class);
+
+$login = function () {
+    $this->validate();
+
+    $this->form->authenticate();
+
+    Session::regenerate();
+
+    $this->redirectIntended(default: route('dashboard', absolute: false), navigate: true);
+};
+
+?>
+
+<div>
+</div>
